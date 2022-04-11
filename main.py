023dc88d705928f8fec0f51b1aa2ce87c6808e2c
ShivamTyagi72 <tyagishivam076@gmail.com>
2022-04-11 13:19:25 +0530

    adding files.

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app=FastAPI()
 
 
 
-@app.get('/')
-def index():
-    return {"data":{"name":"shivam"}}
+@app.get('/blog')
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
+
+    
+
+    if published:
+        return {'data' : f'{limit} published blogs from the db'}
+
+    else:
+        return {'data':f'{limit} blogs from the db'}
+
+    
+
+      
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data":"all unpublished blogs"}
 
 
 
-@app.get("/about")
-def about():
-    return {"data":"about page"}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return {"data":id}
+
+
+
+
+
+@app.get("/blog/{id}/comments")
+def comments(id:int,limit=10):
+    return {"data":{"1","2"}}
+
+
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]
+
+
+
+
+
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    
+    return {'data':f'blog is created with title {blog.title}'}
